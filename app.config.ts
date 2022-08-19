ebb319484196785a1a35aa9cb247c9c2c73e777d
adonis <adonisdoda@gmail.com>
2022-08-19 15:28:25 -0300

    novo versioncode android

--- app.config.ts
+++ app.config.ts
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 	},
 	android: {
 		package: "app.klubbs.establishment",
-		versionCode: 4,
+		versionCode: 5,
 		adaptiveIcon: {
 			foregroundImage: './assets/images/android-icon.png',
 			backgroundColor: '#1F1F1F'
