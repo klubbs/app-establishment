b4c8a8bd668b0f5a6d44d775c46405c7bee18272
adonis <adonisdoda@gmail.com>
2023-07-27 01:49:30 -0300

    implementing credit card save screen

--- src/components/screens/payments/index.tsx
+++ src/components/screens/payments/index.tsx
@@ -1,37 +1,81 @@
 import React, { useState } from 'react';
-import { View, SafeAreaView } from 'react-native';
-import { CVCCardInput, CreditCardNumber, DateCardInput, NameUserInput } from './styles';
-
+import { View, KeyboardAvoidingView } from 'react-native';
+import { CVCCardInput, CardBackground, CardTexts, CreditCardNumber, DateCardInput, NameUserInput, SaveButton } from './styles';
 // import { Container } from './styles';
 
 const PaymentsScreen: React.FC = () => {
-	const [userNameCard, setUserNameCard] = useState('')
-	const [date, setDate] = useState('')
-	const [cvc, setCvc] = useState('')
 
+	const [userNameCard, setUserNameCard] = useState('Adonis Antunes Uessler')
+	const [creditCardNumber, setCreditCardNumber] = useState('5214330298188709')
+	const [date, setDate] = useState('2406')
+	const [cvc, setCvc] = useState('367')
+
+	const [editable, setEditable] = useState(false)
+
+	const BUTTON_TEXT = editable ? 'SALVAR' : 'ALTERAR CARTÃO'
+	const PLACEHOLDER_NAME = editable ? userNameCard : userNameCard.replace(/[a-zA-Z0-9]/g, '*')
+	const PLACEHOLDER_DATE = editable ? date : '** / **'
+	const PLACEHOLDER_CVC = editable ? cvc : '***'
+	const PLACEHOLDER_NUMBER = editable ? creditCardNumber : creditCardNumber.slice(-4)
+
+	function handleSave() {
+		//will convert just in next render
+		setEditable(!editable)
+
+		if (!editable) {
+			setUserNameCard('')
+			setCreditCardNumber('')
+			setDate('')
+			setCvc('')
+			return
+		}
+
+
+	}
+
+	function showWhenMoreThan12() {
+		if (creditCardNumber.length > 17) {
+			return creditCardNumber.slice(-4)
+		}
+
+		return ''
+	}
 
 	return (
 		<View style={{ flex: 1, padding: 10 }} >
-			<View style={{ flex: 1 }}>
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<CardBackground>
+					<CardTexts>●●●● ●●●● ●●●● {showWhenMoreThan12()}</CardTexts>
+					<CardTexts>	CARTÃO DE CRÉDITO</CardTexts>
+				</CardBackground>
 			</View>
-			<View style={{ alignItems: 'center', flex: 1, rowGap: 10, justifyContent: 'center' }}>
+			<KeyboardAvoidingView behavior={'padding'} style={{
+				alignItems: 'center', flex: 1, rowGap: 10, justifyContent: 'center'
+			}}>
+				<SaveButton onPress={handleSave} text={BUTTON_TEXT} enable={editable} />
 				<NameUserInput
-					value={userNameCard}
+					editable={editable}
+					value={PLACEHOLDER_NAME}
 					setValue={setUserNameCard}
 				/>
-				<CreditCardNumber />
+				<CreditCardNumber
+					editable={editable}
+					value={PLACEHOLDER_NUMBER}
+					onChangeText={setCreditCardNumber} />
 				<View style={{ flexDirection: 'row', columnGap: 10 }}>
 					<DateCardInput
-						value={date}
+						editable={editable}
+						value={PLACEHOLDER_DATE}
 						setValue={setDate}
 					/>
 					<CVCCardInput
-						value={cvc}
+						editable={editable}
+						value={PLACEHOLDER_CVC}
 						setValue={setCvc}
 					/>
 				</View>
-			</View>
-		</View>
+			</KeyboardAvoidingView>
+		</View >
 	);
 }
 
--- src/components/screens/payments/styles.ts
+++ src/components/screens/payments/styles.ts
@@ -2,15 +2,17 @@ import styled from 'styled-components/native'
 import { InputWithMask } from '../../component/input-with-mask';
 import colors from '../../../../assets/constants/colors';
 import { InputWithIcon } from '../../component/input-with-icon';
+import Button from '../../component/button';
 
 
-export const CreditCardNumber = styled(InputWithMask).attrs({
+export const CreditCardNumber = styled(InputWithMask).attrs<{ editable: boolean }>((({ editable }) => ({
 	type: "credit-card",
 	placeholder: 'Número do cartão',
+	placeholderTextColor: colors.COLOR_BLACK50,
 	options: {
-		obfuscated: false
+		obfuscated: editable ? false : true
 	},
-})`
+})))`
 	text-align: center;
 	width: 100%;
 	height:60px;
@@ -29,7 +31,8 @@ export const NameUserInput = styled(InputWithIcon).attrs(({
 `;
 
 export const DateCardInput = styled(InputWithIcon).attrs(({
-	placeholder: 'MÊS/ANO'
+	placeholder: 'MÊS/ANO',
+	inputMode: 'numeric'
 }))`
 	text-align: center;
 	width: 49%;
@@ -39,11 +42,31 @@ export const DateCardInput = styled(InputWithIcon).attrs(({
 `
 
 export const CVCCardInput = styled(InputWithIcon).attrs(({
-	placeholder: 'CVC'
+	placeholder: 'CVC',
+	inputMode: 'numeric'
 }))`
 	text-align: center;
 	width: 49%;
 	height:60px;
 	background-color: ${colors.COLOR_SECUNDARY_WHITE};
 	color: ${colors.COLOR_SECUNDARY_BLACK};
+`
+export const SaveButton = styled(Button).attrs<{ enable: boolean }>(({ enable }) => ({
+	styleContainer: { backgroundColor: enable ? colors.COLOR_GREEN : colors.COLOR_YELLOW },
+	styleButton: { color: enable ? colors.COLOR_SECUNDARY_GREEN : colors.COLOR_YELLOW_BUTTON_TEXT }
+}))`
+`
+export const CardBackground = styled.View`
+	background-color: ${colors.COLOR_SECUNDARY_BLACK} ;
+	border-radius: 15px;
+	width: 90%;
+	height: 45% ;
+	padding:15px;
+	justify-content: flex-end;
+	row-gap: 50px;
+`
+export const CardTexts = styled.Text`
+	color: ${colors.COLOR_SECUNDARY_WHITE};
+	font-size: 15px;
+  font-family:'Nunito_Bold';
 `
